d7af4ef75fc349fc536407ff5093ef27b7233835
Richard Yance <yance0022@gmail.com>
2019-05-24 19:37:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,15 +31,20 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
   end
 
-  get '/articles/:id/edit' do
-    @article = Article.find(params[:id])
-    erb :edit
-  end
+  delete '/articles/:id' do 
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end 
 
 end
